3a7846553e152854680682a1cb6746cf9ef482cf
Leonardo Correa Ferreira <110972696+leonardocofe@users.noreply.github.com>
2023-04-17 21:27:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+const inputTarefa =  document.querySelector('.input-nova-tarefa');
+const bntTarefa = document.querySelector('.btn-nova-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi(){
+  const li = document.createElement('li');
+  return li;
+}
+
+inputTarefa.addEventListener('keypress', function(e){
+  if(e.keyCode == 13){
+    if(!inputTarefa.value) return;
+    criarTarefa(inputTarefa.value);
+    }
+}
+)
+
+function limparInput(){
+  inputTarefa.value = '';
+  inputTarefa.focus();
+}
+
+function criarBotaoApagar(li){
+  li.innerText += ' ';
+  const botaoApagar = document.createElement('button');
+  botaoApagar.innerText = 'Apagar';
+  botaoApagar.setAttribute('class', 'apagar');
+  li.appendChild(botaoApagar);
+}
+
+function criarTarefa(textoInput){
+  const li = criaLi();
+  li.innerText = textoInput;
+  tarefas.appendChild(li);
+  limparInput();
+  criarBotaoApagar(li);
+  salvarTarefas();
+}
+
+bntTarefa.addEventListener('click', function(){
+  if(!inputTarefa.value) return;
+  criarTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function(e){
+  const el = e.target;
+
+  if(el.classList.contains('apagar')){
+    el.parentElement.remove();
+    salvarTarefas();
+  }
+});
+
+function salvarTarefas(){
+  const liTarefas = tarefas.querySelectorAll('li');
+  const listaDeTarefas = [];
+
+  for(let tarefa of liTarefas){
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace('Apagar', ' ');
+    listaDeTarefas.push(tarefaTexto);
+  }
+  const tarefasJSON = JSON.stringify(listaDeTarefas);
+  localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas(){
+  const tarefas = localStorage.getItem('tarefas');
+  const listaDeTarefas = JSON.parse(tarefas);
+}
+adicionaTarefasSalvas();
